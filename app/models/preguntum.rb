9469d2ca495b326f45a57e18c76ee7b0f9f68ff3
Juan Ortiz <56034091+jorti01@users.noreply.github.com>
2023-05-24 17:03:00 -0500

    Update preguntum.rb

--- app/models/preguntum.rb
+++ app/models/preguntum.rb
@@ -1,2 +1,14 @@
+require 'net/http'
+
+http = Net::HTTP.new('18.235.214.57', 3001)
+request = Net::HTTP::Get.new('/api/v1/topics')
+response = http.request(request)
+topics = JSON.parse(response.body)
+$temas = []
+topics["topics"].each do |topic|
+  $temas.append(topic["name"])
+end
+
 class Preguntum < ApplicationRecord
+    validates_inclusion_of :tema, in: $temas
 end
