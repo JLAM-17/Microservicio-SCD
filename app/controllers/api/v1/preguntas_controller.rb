face42112e1f4e0ae0baf347d383c769b206e64a
Juan Ortiz <56034091+jorti01@users.noreply.github.com>
2023-05-31 07:56:08 -0500

    Error Handling / fixes

--- app/controllers/api/v1/preguntas_controller.rb
+++ app/controllers/api/v1/preguntas_controller.rb
@@ -8,39 +8,54 @@ class Api::V1::PreguntasController < ApplicationController
 
   def validar_respuesta
   @preguntum = Preguntum.find(params[:id])
-  @respuesta = sanitize(params[:respuesta])
+  @respuesta = params[:respuesta]
   numero = (@preguntum.respuesta_correcta.to_s)
   @is_correct = @preguntum.send("respuesta_#{numero}").downcase == @respuesta.downcase
   if @is_correct
     @answer = "Correcta"
-  else 
+  else
     @answer = "Incorrecta"
   end
   render json: { "ID Pregunta": params[:id],"Resultado": @answer }
 
-  end  
+  end
   def generar_preguntas
-    num_preguntas = sanitize(params[:cantidad].to_i)
-    tema = sanitize(params[:tema])
+    num_preguntas = params[:cantidad].to_i
+    tema = params[:tema]
     @questions = Preguntum.where(tema: tema).sample(num_preguntas)
-    render json: @questions, :except => :respuesta_correcta
+    if !Preguntum.where(tema: tema).sample(num_preguntas).blank?
+      render json: @questions, :except => :respuesta_correcta, status: :found
+    else
+      render json: { message: "Preguntas con ese tema no fueron encontradas!" }, status: :bad_request
+    end
   end
   def create
     pregunta = Preguntum.create(pregunta_params)
     if pregunta.save
       render json: { message: "Pregunta creada!" }, status: :created
     else
-      render json: { errors: @resource.errors.full_messages }, status: :unprocessable_entity
+      render json: { message: "Pregunta no fue creada!" }, status: :bad_request
     end
     # render json: { message: "Pregunta guardada!" }, status: :created
   end
   def update
-    pregunta = Preguntum.find(params[:id])
-    pregunta.update_attributes(pregunta_params)
-    render json: pregunta
+    @pregunta = Preguntum.find(params[:id])
+    if @pregunta.update(pregunta_params)
+      render json: @pregunta
+    else
+      render json: { error: "Failed to update pregunta" }, status: :bad_request
+    end
   end
   def destroy
-  Preguntum.destroy(params[:id])
+    begin
+      pregunta = Preguntum.find(params[:id])
+      pregunta.destroy
+      render json: { message: "Pregunta eliminada: #{pregunta.id}" }, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: "Pregunta no encontrada" }, status: :not_found
+    rescue => e
+      render json: { message: "Ocurrió un error al eliminar la pregunta: #{e.message}" }, status: :internal_server_error
+    end
   end
   private
 
